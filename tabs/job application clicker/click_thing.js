e734633c061adf04f951a2d692b8c229071173f3
me-zoen <zvehonsky000@student.rcschools.net>
2025-11-07 13:11:29 -0600

    Update click_thing.js

--- tabs/job application clicker/click_thing.js
+++ tabs/job application clicker/click_thing.js
@@ -1,14 +1,24 @@
 const click_thing = document.getElementById("click_thing");
-const job_applications_filled_lable = document.getElementById("job_applications_filled_lable");
+const job_applications_filled_label = document.getElementById("job_applications_filled_label");
 
 let job_applications_filled = localStorage.getItem("job_applications_filled");
 
+if (job_applications_filled === null) {
+    job_applications_filled = 0;
+} else {
+    job_applications_filled = parseInt(job_applications_filled, 10);
+}
 
 function handleClick() {
   job_applications_filled += 1;
-  job_applications_filled_lable.textContent = `job applications filled: ${job_applications_filled}`
+
+  job_applications_filled_label.textContent = `job applications filled: ${job_applications_filled}`;
+
   localStorage.setItem("job_applications_filled", job_applications_filled);
 }
 
+click_thing.addEventListener("click", handleClick);
 
-imageElement.addEventListener("click_thing", on_click);
+
+job_applications_filled_label.textContent = `job applications filled: ${job_applications_filled}`;
+
